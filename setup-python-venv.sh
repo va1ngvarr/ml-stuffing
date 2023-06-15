00ef968359bca53e9f032053794e5c09dd40b201
Igor Vasiljev <va1ngvarr@gmail.com>
2023-06-15 17:46:34 +0500

    Update setup-python-venv.sh

--- setup-python-venv.sh
+++ setup-python-venv.sh
@@ -10,11 +10,22 @@ sudo apt-get update && sudo apt-get install -y git python3 python3-pip python3-v
 export DS_BUILD_CPU_ADAM=1
 export DS_BUILD_SPARSE_ATTN=1
 
-export DS_BUILD_OPS=0
+export DS_BUILD_OPS=1
 
-pip install --upgrade pip && pip install torch==1.8.2 \
-    torchvision==0.9.2 torchaudio==0.8.2 triton==1.0.0 \
-    --extra-index-url https://download.pytorch.org/whl/lts/1.8/cu111
+# CUDA 11.1
+# pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
+
+# CUDA 10.2
+# pip install torch==1.8.1+cu102 torchvision==0.9.1+cu102 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
+
+# CUDA 10.1
+# pip install torch==1.8.1+cu101 torchvision==0.9.1+cu101 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
+
+pip install --upgrade pip && pip install torch torchvision torchaudio 
+
+# pip install torch==1.8.2 \
+#    torchvision==0.9.2 torchaudio==0.8.2 triton==1.0.0 \
+#    --extra-index-url https://download.pytorch.org/whl/lts/1.8/cu111
 
 pip install -r requirements.txt
 
