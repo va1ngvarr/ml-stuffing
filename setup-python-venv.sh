83c4615586fc2bfdb7187d9f00e8237d1eaf5d7c
Igor Vasiljev <va1ngvarr@gmail.com>
2023-06-15 20:19:59 +0500

    Update setup-python-venv.sh

--- setup-python-venv.sh
+++ setup-python-venv.sh
@@ -1,25 +1,12 @@
 #!/bin/sh
 
-sudo apt-get update && sudo apt-get install -y python3 python3-pip python3-venv
-
-[ -d "venv/" ] && echo Virtual environment is already created. Keep it
-[ ! -d "venv/" ] && python3 -m venv venv/
 . venv/bin/activate
 
-# CUDA 11.1
-# pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
-
-# CUDA 10.2
-# pip install torch==1.8.1+cu102 torchvision==0.9.1+cu102 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
-
-# CUDA 10.1
-# pip install torch==1.8.1+cu101 torchvision==0.9.1+cu101 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
+# 
 
 pip install torch==1.8.2 \
     torchvision==0.9.2 torchaudio==0.8.2 triton==1.0.0 \
     --extra-index-url https://download.pytorch.org/whl/lts/1.8/cu111
-
-pip install -r requirements.txt
 
 
 # PORT=5000
