c0f8ea420636e793b11838063060429390f60b67
Igor Vasiljev <va1ngvarr@gmail.com>
2023-06-15 16:37:46 +0500

    Update setup-python-venv.sh

--- setup-python-venv.sh
+++ setup-python-venv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get update && sudo apt-get install git python3 python3-pip python3-venv \ 
+sudo apt-get update && sudo apt-get install git python3 python3-pip python3-venv \
      ninja-build cmake clang-9 llvm-9 llvm-9-dev llvm-9-tools
 
 [ ! -d "venv/" ] && python3 -m venv venv/
@@ -13,5 +13,9 @@ pip install --upgrade pip
 pip install torch==1.9 triton==1.0.0 && pip install -r requirements.txt
 
 git clone https://github.com/NVIDIA/apex && cd apex
-pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings \ 
+pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings \
     "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./
+
+PORT=5000
+# iptables -A INPUT -p tcp --dport $(PORT) -j ACCEPT
+# jupyter notebook --ip 0.0.0.0 --no-browser --port=$(PORT)
