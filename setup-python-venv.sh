dbad1dea95c5a9ee03e555443bde5f0a2bf385a4
Igor Vasiljev <va1ngvarr@gmail.com>
2023-06-15 20:26:43 +0500

    Update setup-python-venv.sh

--- setup-python-venv.sh
+++ setup-python-venv.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-. venv/bin/activate
-
-# 
-
-pip install torch==1.8.2 \
-    torchvision==0.9.2 torchaudio==0.8.2 triton==1.0.0 \
-    --extra-index-url https://download.pytorch.org/whl/lts/1.8/cu111
-
-
 # PORT=5000
 # iptables -A INPUT -p tcp --dport $(PORT) -j ACCEPT
 # jupyter notebook --ip 0.0.0.0 --no-browser --port=$(PORT)
