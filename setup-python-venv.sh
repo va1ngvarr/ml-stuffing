0f0fe1dba268a15129233cc26c5a7694d4ab7e50
Igor Vasiljev <va1ngvarr@gmail.com>
2023-06-15 17:55:15 +0500

    Update setup-python-venv.sh

--- setup-python-venv.sh
+++ setup-python-venv.sh
@@ -21,7 +21,6 @@ export DS_BUILD_OPS=1
 # CUDA 10.1
 # pip install torch==1.8.1+cu101 torchvision==0.9.1+cu101 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
 
-pip uninstall torch torchvision torchaudio 
 pip install --upgrade pip && pip install torch torchvision torchaudio 
 
 # pip install torch==1.8.2 \
